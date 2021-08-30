312c465cc840080fa9d13bf9f9f3c05a3027bd7e
KarpoVelho <40266114+KarpoVelho@users.noreply.github.com>
2021-08-30 20:02:31 +0300

    Create Config.lua

---
+++ Config.lua
@@ -0,0 +1,18 @@
+Config = {}
+Config.boliiseja = 0 --paljo kyttii alotuksee
+Config.aseita = true --onks mahollisuus saada aseita(pieni chance)
+Config.kuinkauan = 35 --sekunteina
+Config.cooldown = 15 --minuutteina, yhteinen cooldown
+
+Config.itemit = {
+    "luotiliivi",
+    "lipas",
+    "silu"
+}
+
+
+Config.aseet = {
+    [1] = "WEAPON_SNSPISTOL_MK2",
+    [2] = "WEAPON_PISTOL_MK2",
+    [3] = "WEAPON_VINTAGEPISTOL"
+}
